f8496136672bf46b56e5653e2a84c14146e1e2f3
sawyerprecious <sawyerprecious@gmail.com>
2019-05-07 19:23:20 -0700

    Gameplay working for the most part

--- creature.py
+++ creature.py
@@ -1,30 +1,76 @@
 import pygame
 
+vec = pygame.math.Vector2
+
+
 class Creature:
 
-    def __init__(self, x, y, size):
-        self.x = x
-        self.y = y
+    def __init__(self, pos, size):
         self.size = size
         self.health = 0
+        self.energy = 0
+        self.foodCollected = 0
         self.img = None
+        self.vel = vec(0, 0)
+        self.acceleration = vec(0, -0.2)
+        self.angle_speed = 0
+        self.angle = 0  # TODO: point to center of screen
+        self.position = vec(pos[0], pos[1])
+        self.home = False
+
+    def friction(self):
+        self.vel.x = self.vel.x - (self.vel.x / 30)
+        self.vel.y = self.vel.y - (self.vel.y / 30)
 
     def draw(self, win):
         # draws creature
-        win.blit(self.img, (self.x, self.y))
+        # win.blit(self.img, (self.x, self.y))
+        # pygame.draw.circle(win, (0, 0, 255), (int(self.position.x), int(self.position.y)), 5, 0)
+        # pygame.display.update()
         self.move()
-        pass
 
     def collide(self, x, y):
         # checks collision
-        if x <= self.x + self.size and x >= self.x:
-            if y <= self.y + self.size and y >= self.y:
+        if self.position.x + self.size >= x >= self.position.x - self.size:
+            if self.position.y + self.size >= y >= self.position.y - self.size:
                 return True
         return False
 
     def move(self):
         # moves creature
-        pass
+
+        if not self.home:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                self.angle_speed = -4
+                self.rotate()
+            if keys[pygame.K_RIGHT]:
+                self.angle_speed = 4
+                self.rotate()
+            # If up or down is pressed, accelerate by
+            # adding the acceleration to the velocity vector.
+            if keys[pygame.K_UP]:
+                self.vel += self.acceleration
+            if keys[pygame.K_DOWN]:
+                self.vel -= self.acceleration
+
+            self.position += self.vel
+
+            if self.position.x < 0 or self.position.x > 1200 or self.position.y < 0 or self.position.y > 750:
+                self.position.x = max(self.position.x, 0)
+                self.position.y = max(self.position.y, 0)
+                self.position.x = min(self.position.x, 1200)
+                self.position.y = min(self.position.y, 750)
+                self.got_home()
+
+    def rotate(self):
+        # Rotate the acceleration vector.
+        self.acceleration.rotate_ip(self.angle_speed)
+        self.angle += self.angle_speed
+        if self.angle > 360:
+            self.angle -= 360
+        elif self.angle < 0:
+            self.angle += 360
 
     def hit(self):
         self.health -= 1
@@ -33,4 +79,12 @@ class Creature:
         else:
             return False
 
+    def got_home(self):
+        self.vel = vec(0, 0)
+        self.home = True
+        # if self.foodCollected == 0:
+        #     # die
+        # else:
+        #     # reproduction
+        pass
 
--- game.py
+++ game.py
@@ -1,15 +1,20 @@
 import pygame
 import os
+import creature
+import random
+
 
 class Game:
 
     def __init__(self):
-        self.width = 1000
-        self.height = 700
+        self.width = 1200
+        self.height = 750
         self.win = pygame.display.set_mode((self.width, self.height))
         self.creatures = []
         self.food_items = []
         self.bg = pygame.image.load(os.path.join("assets", "background.png"))
+        self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
+        self.create_food()
 
     def run(self):
         run = True
@@ -22,10 +27,38 @@ class Game:
                 if event.type == pygame.QUIT:
                     run = False
 
+                pos = pygame.mouse.get_pos()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    c = creature.Creature(pos, 15)
+                    self.creatures.append(c)
+
+            for c in self.creatures:
+                c.draw(self.win)
+
             self.draw()
 
         pygame.quit()
 
     def draw(self):
         self.win.blit(self.bg, (0, 0))
+        for c in self.creatures:
+            c.friction()
+            pygame.draw.circle(self.win, (255, 0, 0), (int(getattr(c, "position").x), int(getattr(c, "position").y)), 15, 0)
+            for f in self.food_items:
+                if c.collide(f[0], f[1]):
+                    self.food_items.remove(f)
+        for f in self.food_items:
+            pygame.draw.circle(self.win, (0, 255, 0), (f[0], f[1]), 5, 0)
         pygame.display.update()
+
+    def create_food(self):
+        i = 0
+
+        while i < 100:
+            self.food_items.append((random.randint(50, 1150), random.randint(50, 700)))
+            i += 1
+
+
+g = Game()
+g.run()
